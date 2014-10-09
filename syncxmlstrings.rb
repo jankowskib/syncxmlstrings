652e94a85cff1f63f10ae128bd1e9d61b4c21697
Bartosz J <thug1337@gmail.com>
2014-10-09 13:51:26 +0200

    Replace strings by default Add 'clean' and 'just-add-missing' options

--- syncxmlstrings.rb
+++ syncxmlstrings.rb
@@ -5,11 +5,11 @@ require 'colorize'
 require 'fileutils'
 
 def syncxmlfile(fin, fout, keys, fref = nil)
-
+	
+	folder = /^.*(values.*)\//.match(fin)[1]
 	if not File.exists?(fin) or not File.exists?(fout) then
-		puts "Cannot open the file!"
-		""
-		return
+		print "#{folder} [NO_FILE] ".red
+		return {}
 	end
 
 	strs = {}
@@ -38,10 +38,16 @@ def syncxmlfile(fin, fout, keys, fref = nil)
 	end
 			
 	strs.each do |s,c|
+		if $options[:clean] == true && !refstrs.has_key?(s) then
+			puts "[R] " + s + ": '" + c[:content] + "'" if $options[:verbose]
+			strs.delete(s)
+			next
+		end
+		
 		next if !newstrs.has_key?(s)
 		if $options[:ask] == true && newstrs[s] != strs[s] then
-			puts "#{item["name"]}:".green
-			print "#{orgkeys[s]} -> #{newstrs[s]} [y/n/a]: "
+			puts "#{s}:".green
+			print "#{c[:content]} -> #{newstrs[s][:content]} [y/n/a]: "
 			prompt = gets.upcase
 			if(prompt == "A\n")
 				puts "Adding remaining changes..."
@@ -51,11 +57,12 @@ def syncxmlfile(fin, fout, keys, fref = nil)
 			end
 		end
 		
-		if newstrs[s] != strs[s] && $options[:replace] == true
+		if newstrs[s] != strs[s] && $options[:replace] == false
 			puts "[M] " + s + ": '" + c[:content] + "' -> '" + newstrs[s][:content] + "'" if $options[:verbose]
 			strs[s] = newstrs[s]
 			changes+=1
 		end
+		
 	end
 	
 	newstrs.each do |s,c|	
@@ -69,10 +76,10 @@ def syncxmlfile(fin, fout, keys, fref = nil)
 	end
 
 	if changes>0
-		puts "#{changes}/#{entries} changed...".yellow
+		print "#{folder} [#{changes}/#{entries}] ".yellow 
 		strs
 	else
-		""
+		{}
 	end
 end
 
@@ -84,13 +91,15 @@ $options[:replace] = false
 $options[:verbose] = false
 $options[:dirmode] = false
 $options[:look] = false
+$options[:quick] = false
+$options[:clean] = false
 
 OptionParser.new do |opts|
 	opts.banner = "Usage:\n
 To sync single file: syncxmlstrings.rb [options] --i[nput] <file.xml> --o[utput] <file.xml>
 	or
 To sync every strings from directory: syncxmlstrings.rb [options] --f[rom] <directory> --t[o] <directory>
-options: --[v]erbose --[a]sk, --[r]eplace, --[m]aster <file.xml>, --[l]ook-for-master (dir sync mode only), --s[trings] <str1>,<str2> ..."
+options: --[v]erbose --[a]sk, -r --dont-replace, --[m]aster <file.xml>, --[l]ook-for-master (dir sync mode only), --s[trings] <str1>,<str2> ... --[j]ust-add-missing (dir sync mode only) --[c]lean"
 	opts.on("-i", "--input FILE", String, "Input string.xml file where strings will be searched") do |v|
 		$options[:inp] = v
 	end
@@ -112,11 +121,17 @@ options: --[v]erbose --[a]sk, --[r]eplace, --[m]aster <file.xml>, --[l]ook-for-m
 	opts.on("-a", "--ask", "Ask before change") do |v|
 		$options[:ask] = true
 	end
-	opts.on("-r", "--replace", "Replace existing keys") do |v|
+	opts.on("-r", "--dont-replace", "Replace existing keys") do |v|
 		$options[:replace] = true
 	end
-	opts.on("-l", "--look-for-master", "Uses strings.xml from /values/ as reference. --from --to mode only") do |v|
+	opts.on("-l", "--look-for-master", "Uses strings.xml from /values/ as reference. Directory mode only") do |v|
 		$options[:look] = true
+	end
+	opts.on("-j", "--just-add-missing", "Adds only missing strings.xml files. Directory mode only") do |v|
+		$options[:quick] = true
+	end
+	opts.on("-c", "--clean", "Remove strings not included in refrence file. Use with -m or -l") do |v|
+		$options[:clean] = true
 	end
 	opts.on("-v", "--verbose", "Print the changes") do |v|
 		$options[:verbose] = true
@@ -133,6 +148,13 @@ if($options.has_key?(:inp) == false && $options.has_key?(:out) == false)
 	$options[:dirmode] = true
 end
 
+if !$options[:dirmode] && ($options[:quick] || $options[:look])
+	puts "Incorrect option combination"
+	puts "Type syncxmlstrings.rb --help to see usage!"
+	exit
+end
+
+
 puts "Synced strings: " << $options[:strings] if $options[:strings]
 
 if $options[:dirmode] then
@@ -142,14 +164,14 @@ if $options[:dirmode] then
 			puts "Using reference: " + "#{$options[:from]}/values/strings.xml"
 			$options[:ref] = "#{$options[:from]}/values/strings.xml"
 		end
-		
+	puts "Legend: ".colorize(:default) << "up to date ".green << "modified [changes/entries]".yellow << "copied ".blue << "error [type]".red
 	Dir.entries($options[:from]).select { |file| File.directory? File.join($options[:from], file) }.each do |val|
 		if val=~/values.*/ then
-			if File.exists? "#{$options[:to]}/#{val}/strings.xml" then
-				puts "#{val}/strings.xml: Syncing...".green
+			if File.exists?("#{$options[:to]}/#{val}/strings.xml") then
+				next if $options[:quick]
 				strs = syncxmlfile("#{$options[:from]}/#{val}/strings.xml","#{$options[:to]}/#{val}/strings.xml", $options[:strings], $options[:ref])
 					if strs.empty?
-						puts "#{val}/strings.xml: Up to date...".yellow
+						print "#{val} ".green
 						next
 					end
 				File.open("#{$options[:to]}/#{val}/strings.xml", 'wb') do |f| 
@@ -165,13 +187,13 @@ if $options[:dirmode] then
 					f.write("</resources>")
 				end
 			elsif File.exists?("#{$options[:from]}/#{val}/strings.xml")
-				puts "#{val}/strings.xml -> #{$options[:to]}: Copying...".green
-				FileUtils.mkdir "#{$options[:to]}/#{val}/"
+				print "#{val} ".blue
+				FileUtils.mkdir "#{$options[:to]}/#{val}/" if not File.directory?("#{$options[:to]}/#{val}/")
 				FileUtils.cp("#{$options[:from]}/#{val}/strings.xml", "#{$options[:to]}/#{val}/")
 			end
 		end
 	end
-	puts "Done!".green
+	puts "\nDone!".green
 else
 	puts "Input: " << $options[:inp]
 	puts "Output: " << $options[:out]
